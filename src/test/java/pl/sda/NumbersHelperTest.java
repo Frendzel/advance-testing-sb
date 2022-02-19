99633fe588973a16c5eacd7bfa7274e9a02e3e03
Marcin <marcinerbel90@gmail.com>
2022-02-19 13:36:37 +0100

    Workshops - advanced testing parametrized test - positive

--- src/test/java/pl/sda/NumbersHelperTest.java
+++ src/test/java/pl/sda/NumbersHelperTest.java
@@ -1,5 +1,18 @@
 package pl.sda;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class NumbersHelperTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7})
+    @DisplayName("Check if number is odd - positive")
+    public void test(int a) {
+        boolean result = NumbersHelper.isOdd(a);
+        Assertions.assertTrue(result);
+    }
 
 }
