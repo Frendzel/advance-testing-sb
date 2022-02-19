5b9964cac4511393e55a90d02e9173ccbc781cfa
Marcin <marcinerbel90@gmail.com>
2022-02-19 14:00:01 +0100

    Workshops - advanced testing parametrized test - negative & catch exceptions

--- src/main/java/pl/sda/NumbersHelper.java
+++ src/main/java/pl/sda/NumbersHelper.java
@@ -1,13 +1,20 @@
 package pl.sda;
 
-//TODO write parametrized test
-//TODO write simple junit test
 //TODO use @ValueSource
 //TODO pass data from CSV file
 public class NumbersHelper {
+    /**
+     * @param number - number to validate
+     * @return - return odd number
+     * @throws - {@link IllegalArgumentException} - when number is 0
+     */
     public static boolean isOdd(int number) {
+        if (number == 0) {
+            throw new IllegalArgumentException("number is 0!");
+        }
         return number % 2 != 0;
     }
+
     public static float divide(float a, float b) {
         if (b == 0) {
             throw new IllegalArgumentException("dividend can't be 0");
--- src/test/java/pl/sda/NumbersHelperTest.java
+++ src/test/java/pl/sda/NumbersHelperTest.java
@@ -2,7 +2,9 @@ package pl.sda;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NumbersHelperTest {
@@ -10,9 +12,39 @@ class NumbersHelperTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 3, 5, 7})
     @DisplayName("Check if number is odd - positive")
-    public void test(int a) {
+    public void isOddTest(int a) {
+        //when
         boolean result = NumbersHelper.isOdd(a);
+        //then
         Assertions.assertTrue(result);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8})
+    @DisplayName("Check if number is odd - negative")
+    public void isNotOddTest(int a) {
+        //when
+        boolean result = NumbersHelper.isOdd(a);
+        //then
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("Check if number is zero - negative")
+    public void testIfZero(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> NumbersHelper.isOdd(0));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "/home/marcin/sources/advance-testing-sb/src/test/resources/oddFiles.csv")
+    @DisplayName("Check if number is odd - positive")
+    public void isNotOddTestFromFile(int a) {
+        //when
+        boolean result = NumbersHelper.isOdd(a);
+        //then
+        Assertions.assertTrue(result);
+    }
+
+
 
 }
